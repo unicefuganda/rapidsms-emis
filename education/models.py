ee516ba4bd353b5a24e2b6e672d8698db360893b
Assey Alfred <asseym@gmail.com>
2011-07-28 12:25:56 +0300

    xforms and xform fields definitions

--- education/models.py
+++ education/models.py
@@ -1,6 +1,12 @@
 from django.db.signals import post_syncdb
 from .utils import *
 
+def parse_date(command, value):
+    return 1
+
+XFormField.register_field_type('emisdate', 'Date', parse_date,
+                               db_type=XFormField.TYPE_INT, xforms_type='integer')
+
 post_syncdb.connect(init_structures, weak=True)
 post_syncdb.connect(init_xforms, weak=True)
 post_syncdb.connect(init_autoreg, weak=True)
