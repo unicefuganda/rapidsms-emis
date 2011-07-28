7cf284551c6f5930a99f65648291a258194c563d
David McCann <david.a.mccann@gmail.com>
2011-07-28 11:37:30 +0300

    formatting, addition of autoreg script

--- education/utils.py
+++ education/utils.py
@@ -1,5 +1,14 @@
 from django.contrib.auth.models import User
 from rapidsms_xforms.models import Xform, XformField, XformConstraint
+from django.conf import settings
+
+from poll.models import Poll
+from script.models import Script, ScriptStep
+
+try:
+    from django.contrib.sites import Site
+except ImportError:
+    pass
 
 XFORMS = ()
 XFORM_FIELDS = {}
@@ -11,7 +20,133 @@ def init_xforms(sender, **kwargs):
     init_xforms_from_tuples(XFORMS, XFORM_FIELDS)
 
 def init_autoreg(sender, **kwargs):
-    pass
+    script, created = Script.objects.get_or_create(
+            slug="emis_autoreg", defaults={
+            'name':"Education monitoring autoregistration script"})
+    if created:
+        user, created = User.objects.get_or_create(username="admin")
+
+        script.steps.add(ScriptStep.objects.create(
+            script=script,
+            message="Welcome to the SMS based school data collection pilot.The information you provide is valuable to improving the quality of education in Uganda.",
+            order=0,
+            rule=ScriptStep.WAIT_MOVEON,
+            start_offset=0,
+            giveup_offset=60,
+        ))
+        role_poll = Poll.objects.create(\
+            name='emis_role', \
+            user=user, type=Poll.TYPE_TEXT, \
+            question='To register,tell us your role? Teacher, Head Teacher, SMC, GEM, CCT, DEO,or District Official.Reply with one of the roles listed.', \
+            default_response=''
+        )
+        script.steps.add(ScriptStep.objects.create(
+            script=script,
+            poll=roll_poll,
+            order=1,
+            rule=ScriptStep.RESEND_MOVEON,
+            num_tries=1,
+            start_offset=0,
+            retry_offset=86400,
+            giveup_offset=86400,
+        ))
+        district_poll = Poll.objects.create(
+            user=user, \
+            type='district', \
+            name='emis_district',
+            question='What is the name of your district?', \
+            default_response='', \
+        )
+        script.steps.add(ScriptStep.objects.create(
+            script=script,
+            poll=district_poll,
+            order=2,
+            rule=ScriptStep.STRICT_MOVEON,
+            start_offset=0,
+            retry_offset=86400,
+            num_tries=1,
+            giveup_offset=86400,
+        ))
+        county_poll = Poll.objects.create(
+            user=user, \
+            type=Poll.TYPE_TEXT, \
+            name='emis_subcounty',
+            question='What is the name of your sub county?', \
+            default_response='', \
+        )
+        script.steps.add(ScriptStep.objects.create(
+            script=script,
+            poll=county_poll,
+            order=3,
+            rule=ScriptStep.RESEND_MOVEON,
+            start_offset=0,
+            retry_offset=86400,
+            num_tries=1,
+            giveup_offset=86400,
+        ))
+        school1_poll = Poll.objects.create(
+            user=user, \
+            type=Poll.TYPE_TEXT, \
+            name='emis_one_school',
+            question='What is the name of your school?', \
+            default_response='', \
+        )
+        script.steps.add(ScriptStep.objects.create(
+            script=script,
+            poll=school1_poll,
+            order=4,
+            rule=ScriptStep.RESEND_MOVEON,
+            start_offset=0,
+            retry_offset=86400,
+            num_tries=1,
+            giveup_offset=86400,
+        ))
+        schoolmany_poll = Poll.objects.create(
+            user=user, \
+            type=Poll.TYPE_TEXT, \
+            name='emis_many_school',
+            question='Name the schools you are resonsible for.Separate each school name with a comma, for example "St. Mary Secondary,Pader Primary"', \
+            default_response='', \
+        )
+        script.steps.add(ScriptStep.objects.create(
+            script=script,
+            poll=schoolmany_poll,
+            order=5,
+            rule=ScriptStep.RESEND_MOVEON,
+            start_offset=0,
+            retry_offset=86400,
+            num_tries=1,
+            giveup_offset=86400,
+        ))
+        name_poll = Poll.objects.create(
+            user=user, \
+            type=Poll.TYPE_TEXT, \
+            name='emis_name',
+            question='What is your name?', \
+            default_response='', \
+        )
+        script.steps.add(ScriptStep.objects.create(
+            script=script,
+            poll=name_poll,
+            order=6,
+            rule=ScriptStep.RESEND_MOVEON,
+            num_tries=1,
+            start_offset=60,
+            retry_offset=86400,
+            giveup_offset=86400,
+        ))
+        script.steps.add(ScriptStep.objects.create(
+            script=script,
+            message="Welcome to the school monitoring pilot.The information you provide contributes to keeping children in school.",
+            order=7,
+            rule=ScriptStep.WAIT_MOVEON,
+            start_offset=60,
+            giveup_offset=0,
+        ))
+
+        if 'django.contrib.sites' in settings.INSTALLED_APPS:
+            site = Site.objects.get_current()
+            script.sites.add(site)
 
 def init_scripts(sender, **kwargs):
     pass
@@ -19,7 +154,7 @@ def init_scripts(sender, **kwargs):
 def init_xforms_from_tuples(xforms, xform_fields):
     user = User.objects.get(username='admin')
     xform_dict = {}
-    for keyword_prefix,keyword,separator,name,description in xforms:
+    for keyword_prefix, keyword, separator, name, description in xforms:
         xform, created = XForm.objects.get_or_create(
             keyword=keyword,
             keyword_prefix=keyword_prefix,
