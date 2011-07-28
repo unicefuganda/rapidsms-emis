11bef4a256ad58b84d8708045cc59249a6449290
Assey Alfred <asseym@gmail.com>
2011-07-28 12:50:11 +0300

    complete xform definitions

--- education/models.py
+++ education/models.py
@@ -7,6 +7,12 @@ def parse_date(command, value):
 XFormField.register_field_type('emisdate', 'Date', parse_date,
                                db_type=XFormField.TYPE_INT, xforms_type='integer')
 
+def parse_yesno(command, value):
+    return 1
+
+XFormField.register_field_type('emisboolean', 'YesNo', parse_date,
+                               db_type=XFormField.TYPE_INT, xforms_type='integer')
+
 post_syncdb.connect(init_structures, weak=True)
 post_syncdb.connect(init_xforms, weak=True)
 post_syncdb.connect(init_autoreg, weak=True)
