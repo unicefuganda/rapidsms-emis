2bc7f790e3a00637c2e48d1cc366f50f8e137ba1
Assey Alfred <asseym@gmail.com>
2011-07-29 11:00:48 +0300

    date parsing

--- education/models.py
+++ education/models.py
@@ -1,14 +1,49 @@
 from django.db.signals import post_syncdb
+from rapidsms_xforms.models import dl_distance
 from .utils import *
+import re
+import calendar
 
 def parse_date(command, value):
-    return 1
+    try:
+        date_expr = re.compile(r"\d{1,2} (?:%s) \d{2,4}" % '|'.join(calendar.month_abbr[1:]))
+        date_expr1 = re.compile(r"\d{1,2}-\d{1,2}-\d{2,4}")
+        date_expr2 = re.compile(r"\d{1,2}/\d{1,2}/\d{2,4}")
+        date_expr3 = re.compile(r"\d{1,2}\.\d{1,2}\.\d{2,4}")
+        date_expr4 = re.compile(r"\d{2,4}\.\d{1,2}\.\d{1,2}")
+        date_expr5 = re.compile(r"\d{2,4}-\d{1,2}-\d{1,2}")
+
+        if date_expr.match(value):
+            dt_obj = datetime.strptime(date_str, "%d %b %Y")
+        elif date_expr1.match(value):
+            dt_obj = datetime.strptime(value, "%d-%m-%Y")
+        elif date_expr2.match(value):
+            dt_obj = datetime.strptime(value, "%d/%m/%Y")
+        elif date_expr3.match(value):
+            dt_obj = datetime.strptime(value, "%d.%m.%Y")
+        elif date_expr4.match(value):
+            dt_obj = datetime.strptime(value, "%Y.%m.%d")
+        else:
+            dt_obj = datetime.strptime(value, "%Y-%m-%d")
+
+    except ValueError:
+        raise ValidationError("Expected date format "
+                "(\"dd mmm YYYY\", \"dd/mm/yyyy\", \"dd-mm-yyyy\", \"dd.mm.yyyy\", \"yyyy.mm.dd\" or \"yyyy-mm-dd\"), "
+                "but instead received: %s." % value)
+
+    time_tuple = dt_obj.timetuple()
+    timestamp = time.mktime(time_tuple)
+    return int(timestamp)
+
+def parse_yesno(command, value):
+    lvalue = value.lower().strip()
+    if dl_distance(lvalue, 'yes') <= 1 or lvalue == 'y':
+        return 1
+    else:
+        return 0
 
 XFormField.register_field_type('emisdate', 'Date', parse_date,
                                db_type=XFormField.TYPE_INT, xforms_type='integer')
-
-def parse_yesno(command, value):
-    return 1
 
 XFormField.register_field_type('emisboolean', 'YesNo', parse_date,
                                db_type=XFormField.TYPE_INT, xforms_type='integer')
