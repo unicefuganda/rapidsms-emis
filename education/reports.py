3ba0fa55853148970ec484f267b749ac040efba1
Assey Alfred <asseym@gmail.com>
2011-09-14 13:55:06 +0300

    merge changes

--- education/reports.py
+++ education/reports.py
@@ -1,4 +1,33 @@
+from django.db.models import Count
+from generic.reports import Column, Report
+from rapidsms.contrib.locations.models import Location
 from uganda_common.reports import XFormSubmissionColumn, XFormAttributeColumn, PollNumericResultsColumn, PollCategoryResultsColumn, LocationReport
+from uganda_common.utils import total_submissions, reorganize_location, total_attribute_value
+
+GRADES = ['p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7']
+
+class AverageSubmissionBySchoolColumn(Column):
+    def __init__(self, keyword, extra_filters=None):
+        self.keyword = keyword
+        self.extra_filters = extra_filters
+
+    def add_to_report(self, report, key, dictionary):
+        val = total_submissions(self.keyword, report.start_date, report.end_date, report.location, self.extra_filters)
+        for rdict in val:
+            rdict['value'] = rdict['value'] / Location.objects.get(pk=rdict['location_id']).get_descendants(include_self=True).aggregate(Count('schools'))['schools__count']
+        reorganize_location(key, val, dictionary)
+
+
+class AverageAttributeBySchoolColumn(Column):
+    def __init__(self, keyword, extra_filters=None):
+        self.keyword = keyword
+        self.extra_filters = extra_filters
+
+    def add_to_report(self, report, key, dictionary):
+        val = total_attribute_value(self.keyword, report.start_date, report.end_date, report.location, self.extra_filters)
+        for rdict in val:
+            rdict['value'] = rdict['value'] / Location.objects.get(pk=rdict['location_id']).get_descendants(include_self=True).aggregate(Count('schools'))['schools__count']
+        reorganize_location(key, val, dictionary)
 
 
 class MainEmisReport(LocationReport):
@@ -35,3 +64,35 @@ class ClassRoomReport(LocationReport):
 
     classrooms_p7 = XFormAttributeColumn('classrooms_p7')
     classroomsused_p7 = XFormAttributeColumn('classroomsused_p7')
+
+class DashBoardReport(LocationReport):
+    pupil_attendance = XFormAttributeColumn((["boys_%s" % g for g in GRADES] + ["girls_%s" % g for g in GRADES]))
+    teacher_attendance = XFormAttributeColumn("gemteachers_htpresent")
+    total_enrollment = XFormAttributeColumn((["enrolledb_%s" % g for g in GRADES] + ["enrolledg_%s" % g for g in GRADES]))
+    teacher_deployment = XFormAttributeColumn(["deploy_m", "deploy_g"])
+    abuse = XFormAttributeColumn("gemabuse_cases")
+
+class AttendanceReport(LocationReport):
+    boys = XFormAttributeColumn(["boys_%s" % g for g in GRADES])
+    girls = XFormAttributeColumn(["girls_%s" % g for g in GRADES])
+    total_pupils = XFormAttributeColumn((["boys_%s" % g for g in GRADES] + ["girls_%s" % g for g in GRADES]))
+    male_teachers = XFormAttributeColumn("teachers_f")
+    female_teachers = XFormAttributeColumn("teachers_m")
+    total_teachers = XFormAttributeColumn(["teachers_m", "teachers_f"])
+
+class HtAttendanceReport(LocationReport):
+    htattendance = XFormAttributeColumn("gemteachers_htpresent")
+    htdeployment = TotalSchools()
+#    htpercentage = HtPercentage()
+
+class EnrollmentReport(LocationReport):
+    boys = XFormAttributeColumn(["enrolledb_%s" % g for g in GRADES])
+    girls = XFormAttributeColumn(["enrolledg_%s" % g for g in GRADES])
+    total_pupils = XFormAttributeColumn((["enrolledb_%s" % g for g in GRADES] + ["enrolledg_%s" % g for g in GRADES]))
+    male_teachers = XFormAttributeColumn("deploy_f")
+    female_teachers = XFormAttributeColumn("deploy_m")
+    total_teachers = XFormAttributeColumn(["deploy_m", "deploy_f"])
+
+class AbuseReport(LocationReport):
+    gem_abuse = XFormAttributeColumn("gemabuse_cases")
+#    ht_abuse = XFormAttributeColumn("htabuse_cases")
--- education/urls.py
+++ education/urls.py
@@ -1,18 +1,16 @@
-from django.conf.urls.defaults import *
-from uganda_common.utils import get_dates
-from .views import *
-from django.conf.urls.defaults import *
-from generic.views import generic, generic_row, generic_dashboard, generic_map
-from generic.sorters import SimpleSorter, TupleSorter
-from contact.forms import FreeSearchForm, DistictFilterForm, MassTextForm
 from .forms import SchoolFilterForm
-from .sorters import LatestSubmissionSorter
-from rapidsms_xforms.models import XForm, XFormSubmission
-from rapidsms_httprouter.models import Message
 from .models import EmisReporter
-from rapidsms.contrib.locations.models import Location
-from contact.forms import FreeSearchTextForm, DistictFilterMessageForm, HandledByForm, ReplyTextForm
-from .reports import MainEmisReport, ClassRoomReport
+from .reports import MainEmisReport, ClassRoomReport, DashBoardReport, AttendanceReport, HtAttendanceReport, EnrollmentReport, AbuseReport
+from .sorters import LatestSubmissionSorter
+from .views import whitelist, add_connection, delete_connection
+from contact.forms import FreeSearchForm, DistictFilterForm, MassTextForm, \
+    FreeSearchTextForm, DistictFilterMessageForm, HandledByForm, ReplyTextForm
+from django.conf.urls.defaults import patterns, url
+from generic.sorters import SimpleSorter
+from generic.views import generic
+from rapidsms_httprouter.models import Message
+from rapidsms_xforms.models import XFormSubmission
+from uganda_common.utils import get_xform_dates
 
 urlpatterns = patterns('',
    url(r'^emis/messagelog/$', generic, {
@@ -81,7 +79,7 @@ urlpatterns = patterns('',
         'partial_header':'education/partials/partial_header.html',
         'base_template':'generic/timeslider_base.html',
         'needs_date':True,
-        'dates':get_dates,
+        'dates':get_xform_dates,
     }, name='stats'),
 
     url(r'^emis/stats/classroom/$', generic, {
@@ -121,9 +119,136 @@ urlpatterns = patterns('',
         'partial_header':'education/partials/partial_header.html',
         'base_template':'generic/timeslider_base.html',
         'needs_date':True,
-        'dates':get_dates,
+        'dates':get_xform_dates,
     }),
+    url(r'^$', generic, {
+        'model':XFormSubmission,
+        'queryset':DashBoardReport,
+        'selectable':False,
+        'paginated':False,
+        'results_title':None,
+        'columns':[
+            ('', False, '', None),
+            ('pupil attendance', False, '', None),
+            ('teacher attendance', False, '', None),
+            ('total enrollment', False, '', None),
+            ('teacher deployment', False, '', None),
+            ('abuse cases', False, '', None),
+        ],
+        'partial_row':'education/partials/report_row.html',
+        'partial_header':'education/partials/partial_header.html',
+        'base_template':'generic/timeslider_base.html',
+        'needs_date':True,
+        'dates':get_xform_dates,
+    }, name='rapidsms-dashboard'),
     url(r'^emis/whitelist/', whitelist),
     url(r'^connections/add/', add_connection),
     url(r'^connections/(\d+)/delete/', delete_connection),
+
+    url(r'^emis/deo_dashboard/', generic, {
+        'model':XFormSubmission,
+        'queryset':DashBoardReport,
+        'selectable':False,
+        'paginated':False,
+        'results_title':None,
+        'columns':[
+            ('', False, '', None),
+            ('pupil attendance', False, '', None),
+            ('teacher attendance', False, '', None),
+            ('total enrollment', False, '', None),
+            ('teacher deployment', False, '', None),
+            ('abuse cases', False, '', None),
+        ],
+        'partial_row':'education/partials/report_row.html',
+        'partial_header':'education/partials/partial_header.html',
+        'base_template':'generic/timeslider_base.html',
+        'needs_date':True,
+        'dates':get_xform_dates,
+    }, name='deo-dashboard'),
+
+    #attendance table on the DEO dashboard
+    url(r'^emis/attendance/', generic, {
+        'model':XFormSubmission,
+        'queryset':AttendanceReport,
+        'selectable':False,
+        'paginated':False,
+        'results_title':'Attendance',
+        'columns':[
+            ('', False, '', None),
+            ('boys', False, '', None),
+            ('girls', False, '', None),
+            ('total pupils', False, '', None),
+            ('male teachers', False, '', None),
+            ('female teachers', False, '', None),
+            ('total teachers', False, '', None),
+        ],
+        'partial_row':'education/partials/report_row.html',
+        'partial_header':'education/partials/partial_header.html',
+        'base_template':'generic/base.html',
+        'needs_date':True,
+        'dates':get_xform_dates,
+    }, name='attendance-stats'),
+
+    #GEM headteacher attendance report on DEO dashboard
+    url(r'^emis/htattendance/', generic, {
+        'model':XFormSubmission,
+        'queryset':HtAttendanceReport,
+        'selectable':False,
+        'paginated':False,
+        'results_title':'GEM Reported Head Teacher Attendance',
+        'columns':[
+            ('', False, '', None),
+            ('head teacher attendance', False, '', None),
+            ('head teacher deployment', False, '', None),
+            ('percentage attendance', False, '', None),
+        ],
+        'partial_row':'education/partials/report_row.html',
+        'partial_header':'education/partials/partial_header.html',
+        'base_template':'generic/base.html',
+        'needs_date':True,
+        'dates':get_xform_dates,
+    }, name='htattendance-stats'),
+
+    #Enrollment report on DEO dashboard
+    url(r'^emis/enrollment/', generic, {
+        'model':XFormSubmission,
+        'queryset':EnrollmentReport,
+        'selectable':False,
+        'paginated':False,
+        'results_title':'Enrollment Report',
+        'columns':[
+            ('', False, '', None),
+            ('boys', False, '', None),
+            ('girls', False, '', None),
+            ('total enrollment', False, '', None),
+            ('male teachers', False, '', None),
+            ('female teachers', False, '', None),
+            ('total deployment', False, '', None),
+        ],
+        'partial_row':'education/partials/report_row.html',
+        'partial_header':'education/partials/partial_header.html',
+        'base_template':'generic/base.html',
+        'needs_date':True,
+        'dates':get_xform_dates,
+    }, name='enrollment-stats'),
+
+    #Abuse Cases report on DEO dashboard
+    url(r'^emis/abuse/', generic, {
+        'model':XFormSubmission,
+        'queryset':AbuseReport,
+        'selectable':False,
+        'paginated':False,
+        'results_title':'Child Abuse Cases Report',
+        'columns':[
+            ('', False, '', None),
+            ('gem cases', False, '', None),
+            ('head teacher cases', False, '', None),
+        ],
+        'partial_row':'education/partials/report_row.html',
+        'partial_header':'education/partials/partial_header.html',
+        'base_template':'generic/base.html',
+        'needs_date':True,
+        'dates':get_xform_dates,
+    }, name='enrollment-stats'),
+
 )
