0fc1929fbb833d700ccde575916ab921979b7ca2
miclovich <vicmiclovich@gmail.com>
2011-11-01 15:14:34 +0300

    added some modifications for some graphing

--- education/utils.py
+++ education/utils.py
@@ -5,10 +5,16 @@ Created on Sep 15, 2011
 '''
 from script.utils.handling import find_best_response
 from script.models import Script, ScriptSession
-from .models import EmisReporter, School
+from .models import EmisReporter
 from rapidsms.models import Connection
 from datetime import datetime,date
-import xlwt
+
+from contact.models import MessageFlag
+from rapidsms.models import Contact
+from poll.models import Poll
+from script.models import ScriptStep
+from django.db.models import Count
+
 
 def match_connections():
     script = Script.objects.get(slug='emis_autoreg')
@@ -41,21 +47,39 @@ def match_connections():
     #        connection.save()
 
 
-# to quickly generate an excel sheet (some functionality from Uganda commons there)
-def excel_wrapper(transform_model=School):
-    # default model to transform is School
-    book = xlwt.Workbook(encoding='utf8')
-    sheet = book.add_sheet('untitled')
-    default_style = xlwt.Style.default_style
-    datetime_style = xlwt.easyxf(num_format_str='dd/mm/yyyy hh:mm')
-    date_style = xlwt.easyxf(num_format_str='dd/mm/yyyy')
-    values_list = transform_model.objects.all().values_list()
-    for row, rowdata in enumerate(values_list):
-        for col, val in enumerate(rowdata):
-            if isntance(val, datetime):
-                style = datetime_style
-            elif isinstance(val,date):
-                style = date_style
-            else:
-                style = default_style
-            sheet.write(row,col,val,style=style)
+def get_contacts(**kwargs):
+    request = kwargs.pop('request')
+    if request.user.is_authenticated() and hasattr(Contact, 'groups'):
+        return Contact.objects.filter(groups__in=request.user.groups.all()).distinct().annotate(Count('responses'))
+    else:
+        return Contact.objects.annotate(Count('responses'))
+
+def get_polls(**kwargs):
+    script_polls = ScriptStep.objects.exclude(poll=None).values_list('poll', flat=True)
+    return Poll.objects.exclude(pk__in=script_polls).annotate(Count('responses'))
+
+def get_script_polls(**kwargs):
+    script_polls = ScriptStep.objects.exclude(poll=None).values_list('poll', flat=True)
+    return Poll.objects.filter(pk__in=script_polls).annotate(Count('responses'))
+
+#def retrieve_poll(request):
+#    pks=request.GET.get('pks', '').split('+')
+#    if pks[0] == 'l':
+#        return [Poll.objects.latest('start_date')]
+#    else:
+#        pks=[eval(x) for x in list(str(pks[0]).rsplit())]
+#        return Poll.objects.filter(pk__in=pks)
+
+def retrieve_poll(request, pks=None):
+    script_polls = ScriptStep.objects.exclude(poll=None).values_list('poll', flat=True)
+    if pks == None:
+        pks = request.GET.get('pks', '')
+    if pks == 'l':
+        return [Poll.objects.exclude(pk__in=script_polls).latest('start_date')]
+    else:
+        return Poll.objects.filter(pk__in=[pks]).exclude(pk__in=script_polls)
+
+def get_flagged_messages(**kwargs):
+
+    return MessageFlag.objects.all()
+
