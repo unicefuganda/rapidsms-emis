830fa8f670c0326523ac858eb8dcf657e502c91a
Alfred Assey M <asseym@gmail.com>
2011-12-12 10:19:59 +0300

    bug fix in date sorter

--- education/sorters.py
+++ education/sorters.py
@@ -6,18 +6,10 @@ from eav.models import Attribute
 class LatestSubmissionSorter(Sorter):
     def sort(self, column, object_list, ascending=True):
         connections = list(Connection.objects.filter(contact__in=object_list))
-        submissions = list(XFormSubmission.objects.filter(connection__in=connections).order_by('-created').select_related('connection__contact__healthproviderbase__facility','connection__contact__healthproviderbase__location'))
-        full_contact_list = list(object_list)
+        submissions = list(XFormSubmission.objects.filter(connection__in=connections).order_by('-created'))
         toret = []
         for sub in submissions:
-            if not (sub.connection.contact.healthproviderbase in toret):
-                toret.append(sub.connection.contact.healthproviderbase)
-        nosubmissions = []
-        for c in full_contact_list:
-            if not (c in toret):
-                nosubmissions.append(c)
-
-        toret = toret + nosubmissions
+            toret.append(sub.connection.contact.emisreporter)
         if not ascending:
             toret.reverse()
         return toret
